8f923a2faab20e10e7f2b6b649e30174eb751aa8
Nirmala Francis <NFrancis@islandpacific.net>
2017-08-07 10:00:11 +0530

    Initial commit

---
+++ src/test/java/gitTest/GitTest.java
@@ -0,0 +1,23 @@
+package gitTest;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class GitTest {
+
+	@Test
+	public void logintest() throws IOException{
+		WebDriver wd = new FirefoxDriver();
+		wd.get("https://www.google.co.in");
+		File src=((TakesScreenshot)wd).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(src, new File("C:\\Users\\nfrancis.ISLANDPACIFIC\\SeleniumWorkspace\\GitTest\\Screenshot\\google.jpg"));
+	}
+	
+}
